ed987037ed7904ca65f20cd324f45f6ec506e74a
Eunice Dhivya <divi.seed@gmail.com>
2022-02-24 13:16:34 +0530

    add formula and model script

--- caste-equation-up/js/app.js
+++ caste-equation-up/js/app.js
@@ -4,6 +4,11 @@ var chosenCasteSeats = document.getElementById("chosenCasteSeats");
 var error = document.getElementById("error");
 var inputs = [...document.querySelectorAll(".slidecontainer input")];
 
+var blackOut = document.getElementsByClassName("UPCasteEq_model_blackout")[0];
+var modelBox = document.getElementsByClassName("UPCasteEq_model")[0];
+var result = document.getElementById("result");
+var methodology = document.getElementById("methodology");
+
 
 casteSelector.addEventListener("change", function(e){
     console.log(e.target.value);
@@ -13,13 +18,11 @@ casteSelector.addEventListener("change", function(e){
     })
 
     var sumSeats2017 = filterCasteRows.map(function(obj){
-        return obj["TotalSeats2017"];
-    }).reduce(function(a, b){
-        return parseInt(a) + parseInt(b);
+        return obj["Effect"];
     })
 
     chosenCaste.innerHTML = e.target.value;
-    chosenCasteSeats.innerHTML = sumSeats2017;
+    chosenCasteSeats.innerHTML = sumSeats2017[0];
    
 
     console.log(filterCasteRows);
@@ -39,10 +42,12 @@ casteSelector.addEventListener("change", function(e){
 
         // console.log(input.dataset.party);
         // console.log(filterPartyRows[0]["DefaultValues"]);
-        var chosenInputParty = filterPartyRows[0]["DefaultValues"]
+        var chosenInputParty = filterPartyRows[0]["PercentageofVotes(2017)"]
+
+        // console.log(parseFloat(chosenInputParty).toFixed(1), typeof(chosenInputParty));
 
         // input.value = 10;
-        container.querySelector(".dfault-val").textContent = chosenInputParty+"%";
+        container.querySelector(".dfault-val").textContent = parseFloat(chosenInputParty).toFixed(1)+"%";
         container.querySelector(".dfault-val").style.opacity = 1;
         container.querySelector(".dfault-val").style.left = chosenInputParty+"%";
         container.querySelector(".dfault-arrow").style.left = chosenInputParty+"%";
@@ -158,19 +163,99 @@ casteSelector.addEventListener("change", function(e){
             }
 document.getElementById("submit").addEventListener("click", function(){
     var tempCasteVal = casteSelector.value;
+
+    
     // console.log(tempCasteVal);
     if(tempCasteVal === "choose"){
         error.innerHTML = "Caste is required"
     }else{
         var sumOfAllInputs = inputs.reduce((sum, input) => sum + input.valueAsNumber, 0);
         // console.log(sumOfAllInputs);
-        console.log(inputs[0].dataset);
+        // console.log(inputs[0].dataset);
         if(sumOfAllInputs !== 0){
-            console.log("continue to formula");
             error.innerHTML = "";
+
+            var tmpObject = {}
+            for (var input of inputs) {
+                tmpObject[input.dataset.party] = parseInt(input.dataset.value);
+            }
+            
+            
+            var filterCasteRows = data.filter(function(obj){
+                return obj.Caste === tempCasteVal;
+            });
+            
+            // console.log(filterCasteRows);
+            // console.log(tmpObject);
+
+            // var tmpObject = {
+            //     "BJP": 80,
+            //     "SP": 5,
+            //     "BSP": 5,
+            //     "Congress": 1.4,
+            //     "Others": 8.6
+            // }
+
+            formula(tmpObject, filterCasteRows);
+
         }else{
             error.innerHTML = "Make some choices";
         }
     }
 })
+
+
+function formula(sliderInput, datapoints){
+    // console.log("sliderInput", sliderInput);
+    // console.log("keynames", Object.keys(sliderInput));
+    // console.log("datapoint", datapoints);
+    blackOut.style.display = "block";
+    modelBox.style.display = "block";
+    result.style.display = "block";
+    Object.keys(sliderInput).forEach(function(obj){
+        console.log(obj);
+        var filterBasedParty = datapoints.filter(function(obj2){
+            return obj2["Party"] === obj;
+        })
+        var tmpShareOfVotes = sliderInput[obj] / 100 * filterBasedParty[0]["Factor"];
+        var tmpChangeInSeats = Math.round(tmpShareOfVotes / filterBasedParty[0]["Factor"] * filterBasedParty[0]["Effect"])
+        var changefrom2017 = tmpChangeInSeats - parseInt(filterBasedParty[0]["2017Seats"])
+        var finalOutput = changefrom2017 + parseInt(filterBasedParty[0]["TotalSeats2017"])
+
+        var wonlost = changefrom2017 < 0 ? "lost": "won";
+        // var negpos = changefrom2017 < 0 ? "-": "+";
+
+        var str ="<span>"+ obj + "</span> will win <span>" + finalOutput + "</span> seats in 2022 <br><small>(2017 tally: "+filterBasedParty[0]["2017Seats"]+")</small>";
+
+        var str2 = "<h3>"+obj+"</h3><p class='precent'>"+sliderInput[obj]+"%</p><div class='seatsWrap'><p class='seats'>"+finalOutput+"<br><small>SEATS</small></p></div><div class='"+wonlost+"'><p>"+changefrom2017+"<br><span>SEATS</span></p></div>";
+        // var str2 = obj+" "+sliderInput[obj]+"%, "+finalOutput+" SEATS, "+wonlost+changefrom2017+" SEATS";
+
+        // <div id='"+obj.toLowerCase()+"'></div>
+        console.log(str);    
+        console.log(str2);    
+        console.log("");
+        
+        renderDataInModel(str, str2, obj.toLowerCase());
+    })
+}
+
+function renderDataInModel(str, str2, id){
+    document.getElementById(id+"str").innerHTML = str;
+    document.getElementById(id).innerHTML = str2;
+}
+
+
+document.getElementById("closeModel").addEventListener("click", function(){
+    blackOut.style.display = "none";
+    modelBox.style.display = "none";
+    result.style.display = "none";
+    methodology.style.display = "none";
+})
+document.getElementById("closeModel2").addEventListener("click", function(){
+    blackOut.style.display = "none";
+    modelBox.style.display = "none";
+    result.style.display = "none";
+    methodology.style.display = "none";
+})
+
 
